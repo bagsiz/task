d02e72a31c14cb60692e9d8210437e3bc8a60af0
Murat Bağsız <murat@bagsiz.com>
2019-10-25 11:15:35 +0300

    Test 'tasks_can_be_sorted_by_statuses' passing

--- app/Task.php
+++ app/Task.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
+    const TODO = 1;
+    const DOING = 2;
+    const DONE = 3;
+
     protected $fillable = [
       'title', 'description', 'status', 'user_id'
     ];
@@ -13,5 +17,19 @@ class Task extends Model
     public function assignedUser()
     {
         return $this->belongsTo('App\User', 'user_id', 'id');
+    }
+
+    public function scopeSortByStatus($q)
+    {
+        $order = [Task::DOING, Task::TODO, Task::DONE];
+
+        return $q->orderBy('title')
+            ->get()
+            ->sort(function ($a, $b) use($order) {
+                $pos_a = array_search($a->status, $order);
+                $pos_b = array_search($b->status, $order);
+                return $pos_a - $pos_b;
+            }
+        );
     }
 }
